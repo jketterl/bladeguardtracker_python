107ac5d12bf7f26459765c9d03520a2160e95293
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-02-25 21:30:46 +0000

    implement a basic websocket connection using ws4py

--- gpsclient.py
+++ gpsclient.py
@@ -1,9 +1,19 @@
-import gps
+import gps, json
+from ws4py.client.threadedclient import WebSocketClient
+
+class BGTSocket(WebSocketClient):
+	def received_message(self, message):
+		print message
 
 if __name__ == '__main__':
 	session = gps.gps()
 	session.stream(flags=gps.WATCH_JSON)
 
+	socket = BGTSocket('wss://ec2-79-125-69-76.eu-west-1.compute.amazonaws.com/bgt/socket');
+	socket.connect();
+
 	for data in session:
 		if not 'lat' in data or not 'lon' in data or not 'speed' in data: continue
 		print "lat: %f, lon: %f, speed: %f" % (data.lat, data.lon, data.speed);
+
+		socket.send(json.dumps({'command':'log','data':{'lat':data.lat,'lon':data.lon,'eventId':2}}))
