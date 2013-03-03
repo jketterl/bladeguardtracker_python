3efee8c82bbf7fae6ad46c9fc6c0aae3e1634cf3
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-03-03 21:44:00 +0000

    implement gps timeout, gpsUnavailable

--- gpsclient.py
+++ gpsclient.py
@@ -7,7 +7,7 @@ class BGTSocket(WebSocketClient):
 		self.resetTimeout()
 	def resetTimeout(self):
 		def timeout():
-			print "timeout!"
+			print "socket timeout!"
 		if hasattr(self, 'timeout'):
 			self.timeout.cancel()
 		self.timeout = threading.Timer(30, timeout)
@@ -24,20 +24,27 @@ class BGTGPSService(object):
 	def __init__(self, socket):
 		self.socket = socket
 		self.locked = False
+		self.gpsOn = False
 	def start(self):
 		session = gps.gps()
 		session.stream(flags=gps.WATCH_JSON)
 		
 		for data in session:
-			if not 'lat' in data or not 'lon' in data or not 'speed' in data: continue
+			if data['class'] != 'TPV': continue
+
+			if not 'lat' in data or not 'lon' in data or not 'speed' in data:
+				self.sendGPSUnavailable()
+				continue
 			
 			if self.locked: continue
 			self.locked = True
 			self.resetGPSTimeout()
 
+			self.gpsOn = True
 			print "lat: %f, lon: %f, speed: %f" % (data.lat, data.lon, data.speed);
-
 			self.socket.send(json.dumps({'command':'log','data':{'lat':data.lat,'lon':data.lon,'speed':data.speed,'eventId':3}}))
+
+			self.resetSignalTimeout()
 	def resetGPSTimeout(self):
 		def timeout():
 			self.locked = False
@@ -45,6 +52,17 @@ class BGTGPSService(object):
 			self.gpstimeout.cancel();
 		self.gpstimeout = threading.Timer(5, timeout)
 		self.gpstimeout.start()
+	def resetSignalTimeout(self):
+		def timeout():
+			self.sendGPSUnavailable()
+		if (hasattr(self, 'signaltimeout')):
+			self.signaltimeout.cancel();
+		self.signaltimeout = threading.Timer(60, timeout)
+		self.signaltimeout.start()
+	def sendGPSUnavailable(self):
+		if not self.gpsOn: return
+		self.gpsOn = False
+		self.socket.send(json.dumps({'command':'gpsUnavailable','data':{'eventId':3}}))
 
 if __name__ == '__main__':
 	config = ConfigParser.ConfigParser();
